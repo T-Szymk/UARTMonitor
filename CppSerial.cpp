27dd744e17b2523174207514a9ba5012f75f84fe
Ziggy_PC <ziggys_42@hotmail.com>
2020-06-25 21:41:28 +0100

    updated main to filter on time messages

--- CppSerial.cpp
+++ CppSerial.cpp
@@ -5,8 +5,8 @@
 CppSerial::CppSerial()
 {
 
-	memset( &m_OverlappedRead, 0, sizeof( OVERLAPPED ) );
- 	memset( &m_OverlappedWrite, 0, sizeof( OVERLAPPED ) );
+	memset(&m_OverlappedRead, 0, sizeof(OVERLAPPED));
+	memset(&m_OverlappedWrite, 0, sizeof(OVERLAPPED));
 	port_handle = NULL;
 	port_open = FALSE;
 
@@ -19,152 +19,158 @@ CppSerial::~CppSerial()
 
 }
 
-BOOL CppSerial::Open( int nPort, int nBaud )
+BOOL CppSerial::Open(int nPort, int nBaud)
 {
 
-	if( port_open ) return( TRUE );
+	if (port_open) return(TRUE);
 
-	const char* szPort{"\\\\.\\COM1"};
+	const char* szPort{ "\\\\.\\COM4" };
 	LPWSTR szComParams{};
 	DCB dcb;
 
 	//wsprintf( szPort, (LPCWSTR)"\\\\.\\COM%d", nPort );
-	port_handle = CreateFileA(szPort, 
-	              GENERIC_READ | GENERIC_WRITE,
-								0,
-								NULL,
-								OPEN_EXISTING,
-								FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
-								NULL );
-	if( port_handle == NULL ) return( FALSE );
+	port_handle = CreateFileA(szPort,
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		NULL);
+	if (port_handle == NULL) return(FALSE);
 
-	memset( &m_OverlappedRead, 0, sizeof( OVERLAPPED ) );
- 	memset( &m_OverlappedWrite, 0, sizeof( OVERLAPPED ) );
+	memset(&m_OverlappedRead, 0, sizeof(OVERLAPPED));
+	memset(&m_OverlappedWrite, 0, sizeof(OVERLAPPED));
 
 	COMMTIMEOUTS CommTimeOuts;
-	CommTimeOuts.ReadIntervalTimeout = 0xFFFFFFFF;
+	CommTimeOuts.ReadIntervalTimeout = MAXDWORD;
 	CommTimeOuts.ReadTotalTimeoutMultiplier = 0;
 	CommTimeOuts.ReadTotalTimeoutConstant = 0;
 	CommTimeOuts.WriteTotalTimeoutMultiplier = 0;
 	CommTimeOuts.WriteTotalTimeoutConstant = 5000;
-	SetCommTimeouts( port_handle, &CommTimeOuts );
+	SetCommTimeouts(port_handle, &CommTimeOuts);
 
-	m_OverlappedRead.hEvent = CreateEvent( NULL, TRUE, FALSE, NULL );
-	m_OverlappedWrite.hEvent = CreateEvent( NULL, TRUE, FALSE, NULL );
+	// CreateEvent(securityAttribs, manual_reset, initial_state, ptr_to_event_obj_name)
+	m_OverlappedRead.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	m_OverlappedWrite.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
-	dcb.DCBlength = sizeof( DCB );
-	GetCommState( port_handle, &dcb );
+	dcb.DCBlength = sizeof(DCB);
+	GetCommState(port_handle, &dcb);
 	dcb.BaudRate = nBaud;
 	dcb.ByteSize = 8;
+
+	//TODO: Remove ucset and associated constexprs
 	unsigned char ucSet;
-	ucSet = (unsigned char) ( ( FC_RTSCTS & FC_DTRDSR ) != 0 );
-	ucSet = (unsigned char) ( ( FC_RTSCTS & FC_RTSCTS ) != 0 );
-	ucSet = (unsigned char) ( ( FC_RTSCTS & FC_XONXOFF ) != 0 );
-	if( !SetCommState( port_handle, &dcb ) ||
-		!SetupComm( port_handle, 10000, 10000 ) ||
+	ucSet = (unsigned char)((FC_RTSCTS & FC_DTRDSR) != 0);
+	ucSet = (unsigned char)((FC_RTSCTS & FC_RTSCTS) != 0);
+	ucSet = (unsigned char)((FC_RTSCTS & FC_XONXOFF) != 0);
+
+	if (!SetCommState(port_handle, &dcb) ||
+		!SetupComm(port_handle, 10000, 10000) ||
 		m_OverlappedRead.hEvent == NULL ||
-		m_OverlappedWrite.hEvent == NULL ){
+		m_OverlappedWrite.hEvent == NULL) {
 		DWORD dwError = GetLastError();
-		if( m_OverlappedRead.hEvent != NULL ) CloseHandle( m_OverlappedRead.hEvent );
-		if( m_OverlappedWrite.hEvent != NULL ) CloseHandle( m_OverlappedWrite.hEvent );
-		CloseHandle( port_handle );
-		return( FALSE );
-		}
+		if (m_OverlappedRead.hEvent != NULL) CloseHandle(m_OverlappedRead.hEvent);
+		if (m_OverlappedWrite.hEvent != NULL) CloseHandle(m_OverlappedWrite.hEvent);
+		CloseHandle(port_handle);
+		return(FALSE);
+	}
 
 	port_open = TRUE;
 
-	return( port_open );
+	return(port_open);
 
 }
 
-BOOL CppSerial::Close( void )
+BOOL CppSerial::Close(void)
 {
 
-	if( !port_open || port_handle == NULL ) return( TRUE );
+	if (!port_open || port_handle == NULL) return(TRUE);
 
-	if( m_OverlappedRead.hEvent != NULL ) CloseHandle( m_OverlappedRead.hEvent );
-	if( m_OverlappedWrite.hEvent != NULL ) CloseHandle( m_OverlappedWrite.hEvent );
-	CloseHandle( port_handle );
+	if (m_OverlappedRead.hEvent != NULL) CloseHandle(m_OverlappedRead.hEvent);
+	if (m_OverlappedWrite.hEvent != NULL) CloseHandle(m_OverlappedWrite.hEvent);
+	CloseHandle(port_handle);
 	port_open = FALSE;
 	port_handle = NULL;
 
-	return( TRUE );
+	return(TRUE);
 
 }
 
-BOOL CppSerial::WriteCommByte( unsigned char ucByte )
+BOOL CppSerial::WriteCommByte(unsigned char ucByte)
 {
 	BOOL bWriteStat;
 	DWORD dwBytesWritten;
 
-	bWriteStat = WriteFile( port_handle, (LPSTR) &ucByte, 1, &dwBytesWritten, &m_OverlappedWrite );
-	if( !bWriteStat && ( GetLastError() == ERROR_IO_PENDING ) ){
-		if( WaitForSingleObject( m_OverlappedWrite.hEvent, 1000 ) ) dwBytesWritten = 0;
-		else{
-			GetOverlappedResult( port_handle, &m_OverlappedWrite, &dwBytesWritten, FALSE );
+	bWriteStat = WriteFile(port_handle, (LPSTR)&ucByte, 1, &dwBytesWritten, &m_OverlappedWrite);
+	if (!bWriteStat && (GetLastError() == ERROR_IO_PENDING)) {
+		if (WaitForSingleObject(m_OverlappedWrite.hEvent, 1000)) dwBytesWritten = 0;
+		else {
+			GetOverlappedResult(port_handle, &m_OverlappedWrite, &dwBytesWritten, FALSE);
 			m_OverlappedWrite.Offset += dwBytesWritten;
-			}
 		}
+	}
 
-	return( TRUE );
+	return(TRUE);
 
 }
 
-int CppSerial::SendData( const char *buffer, int size )
+int CppSerial::SendData(const char* buffer, int size)
 {
 
-	if( !port_open || port_handle == NULL ) return( 0 );
+	if (!port_open || port_handle == NULL) return(0);
 
 	DWORD dwBytesWritten = 0;
-	int i;
-	for( i=0; i<size; i++ ){
-		WriteCommByte( buffer[i] );
-		dwBytesWritten++;
-		}
 
-	return( (int) dwBytesWritten );
+	for (auto i = 0; i < size; i++) {
+		WriteCommByte(buffer[i]);
+		dwBytesWritten++;
+	}
+
+	return((int)dwBytesWritten);
 
 }
 
-int CppSerial::ReadDataWaiting( void )
+int CppSerial::ReadDataWaiting(void)
 {
 
-	if( !port_open || port_handle == NULL ) return( 0 );
+	if (!port_open || port_handle == NULL) return(0);
 
 	DWORD dwErrorFlags;
 	COMSTAT ComStat;
 
-	ClearCommError( port_handle, &dwErrorFlags, &ComStat );
+	ClearCommError(port_handle, &dwErrorFlags, &ComStat);
 
-	return( (int) ComStat.cbInQue );
+	return((int)ComStat.cbInQue);
 
 }
 
-int CppSerial::ReadData( void *buffer, int limit )
+int CppSerial::ReadData(void* buffer, int bytes_to_read)
 {
 
-	if( !port_open || port_handle == NULL ) return( 0 );
+	if (!port_open || port_handle == NULL) return(0);
 
 	BOOL bReadStatus;
 	DWORD dwBytesRead, dwErrorFlags;
 	COMSTAT ComStat;
 
-	ClearCommError( port_handle, &dwErrorFlags, &ComStat );
-	if( !ComStat.cbInQue ) return( 0 );
+	ClearCommError(port_handle, &dwErrorFlags, &ComStat);
+	if (!ComStat.cbInQue) return(0);
 
-	dwBytesRead = (DWORD) ComStat.cbInQue;
-	if( limit < (int) dwBytesRead ) dwBytesRead = (DWORD) limit;
-
-	bReadStatus = ReadFile( port_handle, buffer, dwBytesRead, &dwBytesRead, &m_OverlappedRead );
-	if( !bReadStatus ){
-		if( GetLastError() == ERROR_IO_PENDING ){
-			WaitForSingleObject( m_OverlappedRead.hEvent, 2000 );
-			return( (int) dwBytesRead );
-			}
-		return( 0 );
+	dwBytesRead = (DWORD)ComStat.cbInQue;
+	if (bytes_to_read < (int)dwBytesRead) {
+		dwBytesRead = (DWORD)bytes_to_read;
 		}
 
-	return( (int) dwBytesRead );
+	bReadStatus = ReadFile(port_handle, buffer, dwBytesRead, &dwBytesRead, &m_OverlappedRead);
+	if (!bReadStatus) {
+		if (GetLastError() == ERROR_IO_PENDING) {
+			WaitForSingleObject(m_OverlappedRead.hEvent, 2000);
+			return((int)dwBytesRead);
+		}
+		return(0);
+	}
+
+	return((int)dwBytesRead);
 
 }
 
--- SerialMain.cpp
+++ SerialMain.cpp
@@ -1,16 +1,19 @@
 
 #include "CppSerial.h"
+#include <iostream>
 
 int main() {
 	CppSerial cppserial;
-	unsigned char buff[4096]{};
-	cppserial.Open(1,9600);
+	unsigned char buff[4096]{0};
+	cppserial.Open(4, 9600);
 
-	for(auto iter = 0; iter < 10; ++iter) {
-		cppserial.ReadData(buff, 4095);
-		Sleep(500);
+	for (auto iter = 0; iter < 200; ++iter) {
+		if(cppserial.ReadData(buff, 4095)) {
+		};
+		
+		
 	}
-	
+
 	cppserial.Close();
 
 	return 0;
