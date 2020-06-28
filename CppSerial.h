f9003036f1c1d3cc01927237d90f180d739cf9d6
Ziggy_PC <ziggys_42@hotmail.com>
2020-06-28 15:45:47 +0100

    - Removed write functionality from CppSerial as it is only used for monitoring. - Tidied syntax so that code is more readable - Updated SerialMain.cpp to test Ublox GPS receiver UART. - Tested successfully.

--- CppSerial.h
+++ CppSerial.h
@@ -7,50 +7,39 @@
 #include <cstdint>
 #include <memory>
 
-using u8 = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
+constexpr uint16_t FC_DTRDSR	= 0x01;
+constexpr uint16_t FC_RTSCTS	= 0x02;
+constexpr uint16_t FC_XONXOFF = 0x04;
+constexpr uint16_t ASCII_BEL  = 0x07;
+constexpr uint16_t ASCII_BS   = 0x08;
+constexpr uint16_t ASCII_LF   = 0x0A;
+constexpr uint16_t ASCII_CR   = 0x0D;
+constexpr uint16_t ASCII_XON  = 0x11;
+constexpr uint16_t ASCII_XOFF = 0x13;
 
-constexpr u16 FC_DTRDSR = 0x01;
-constexpr u16 FC_RTSCTS = 0x02;
-constexpr u16 FC_XONXOFF = 0x04;
-constexpr u16 ASCII_BEL = 0x07;
-constexpr u16 ASCII_BS = 0x08;
-constexpr u16 ASCII_LF = 0x0A;
-constexpr u16 ASCII_CR = 0x0D;
-constexpr u16 ASCII_XON = 0x11;
-constexpr u16 ASCII_XOFF = 0x13;
+class CppSerial
+{
 
-class CppSerial {
+public:
+	CppSerial();
+	~CppSerial();
 
-	public:
-		
-		CppSerial();
-		~CppSerial();
+	BOOL Open( int nPort = 1, int nBaud = 9600 );
+	BOOL Close( void );
 
-		BOOL Open(int nPort = 1, int nBaud = 9600);
-		BOOL Close(void);
+	int ReadData( void *, int );
+	int SendData( const char *, int );
+	int ReadDataWaiting( void );
 
-		int ReadData(void*, int);
-		int SendData(const char*, int);
-		int ReadDataWaiting(void);
+	BOOL IsOpened( void ){ return( port_open ); }
 
-		BOOL IsOpened(void) { return(port_open); }
+protected:
+	BOOL WriteCommByte( unsigned char );
 
-	protected:
-		
-		BOOL WriteCommByte(unsigned char);
+	HANDLE port_handle;
+	OVERLAPPED m_OverlappedRead, m_OverlappedWrite;
+	BOOL port_open;
 
-		HANDLE port_handle;
-		OVERLAPPED m_OverlappedRead, m_OverlappedWrite;
-		BOOL port_open;
-
-	private:
-		// disable copy/move construction/assignment
-		CppSerial(const CppSerial&) = delete;
-		CppSerial(CppSerial&&) = delete;
-		CppSerial& operator=(const CppSerial&) = delete;
-		CppSerial& operator=(CppSerial&&) = delete;
 };
 
 #endif
